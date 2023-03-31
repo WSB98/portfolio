7a0f20f312bac9c5413efd210631231e2fe59e81
Scott <56418482+WSB98@users.noreply.github.com>
2023-03-31 16:50:51 -0400

    initialize portfolio page

---
+++ scripts/clickAndOpen.js
@@ -0,0 +1,58 @@
+/* open the overlay for the clicked project */
+
+
+var projects = document.querySelectorAll('.carousel-slide');
+var overlay = document.getElementById('overlay');
+var carousel = document.querySelector('.carousel-container')
+var nextButton = document.querySelector('.carousel-next')
+var prevButton = document.querySelector('.carousel-prev')
+var spans = document.querySelectorAll('.showTitle')
+var body = document.body;
+var opened = false;
+
+carouselContainer.addEventListener('click', async(e) => {
+    var clickedID = e.target.id;
+
+    
+
+    if(clickedID === '1'){
+        console.log('MSL')
+        overlay.innerHTML = `<a id="close" onclick="closeOverlay()"><svg class='closeIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg></a>
+                            <h3 class="IBM-monospace sectionTitle">My Sales League</h3>`
+    }
+    if(clickedID === '2'){
+        console.log('GVA')
+        overlay.innerHTML = `<a id="close" onclick="closeOverlay()"><svg class='closeIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg></a>
+                            <h3 class="IBM-monospace sectionTitle">Green Valley Web System</h3>`
+    }
+    
+    if(!e.target.contains(nextButton) && !e.target.contains(prevButton) && opened === false){
+        overlay.style.opacity = '100'
+        overlay.style.transform = 'translate(0px,125px)'
+        opened = true;
+    }
+    
+});
+
+
+async function closeOverlay(){
+    overlay.style.transform = 'translate(0px,-1000px)';
+    opened = false
+}
+
+body.addEventListener('click', async(e) => {
+    if(opened === true && (e.target.id === 'psuedoBody' || !overlay.innerHTML.contains(e.target))){
+        spans.forEach(async o => {
+            if(e.target.id === '1'){
+                notInCarousel = false;
+            }
+            else{
+                notInCarousel = true;
+            }
+        })
+        if(notInCarousel){
+            closeOverlay()
+        }
+        console.log('clicked')
+    }
+});
---
+++ scripts/main.js
@@ -0,0 +1,154 @@
+
+window.addEventListener('load', async (e) => {
+  var body = document.body;
+  var colorSelectors = document.getElementById('colorSelectors');
+  var flipper = document.getElementById('swapIcon');
+  var swapper = document.getElementById('colorSwapper');
+  var palette = document.getElementById('paintIcon');
+  var color1 = document.getElementById('color1');
+  var color2 = document.getElementById('color2');
+  var styles = getComputedStyle(document.body);
+  var currFG = styles.getPropertyValue('--background').trim();
+  var currBG = styles.getPropertyValue('--foreground').trim();
+  
+  colorSelectors.style.display = 'none'
+
+  
+  
+
+  if(localStorage.getItem('color1') !== null){
+      color1.value = localStorage.getItem('color1')
+      document.documentElement.style.setProperty("--background", localStorage.getItem('color1'));
+  }
+  else{
+    color1.value = currFG
+  }
+  if(localStorage.getItem('color2') !== null){
+      color2.value = localStorage.getItem('color2')
+      document.documentElement.style.setProperty("--foreground", localStorage.getItem('color2'));
+      
+  } else{
+    color2.value = currBG
+  }
+  
+
+  swapper.addEventListener('click' , async (e) => {
+      
+      swapper.style.width = '150px';
+      palette.style.display = 'none';
+      colorSelectors.style.display = 'flex'
+      
+  });
+
+  document.addEventListener('click', async(e) => {
+      if(!swapper.contains(e.target)){
+        
+          colorSelectors.style.display = 'none'
+          swapper.style.width = '48px';
+          palette.style.display = 'flex';
+          
+      }
+  });
+
+  color1.addEventListener('change', async (e) => {
+      await localStorage.setItem('color1',color1.value)
+      document.documentElement.style.setProperty("--background", localStorage.getItem('color1'));
+  });
+  color2.addEventListener('change', async (e) => {
+      await localStorage.setItem('color2',color2.value)
+      document.documentElement.style.setProperty("--foreground", localStorage.getItem('color2'));
+  });
+  flipper.addEventListener('click', async(e) => {
+    await localStorage.setItem('color1',color2.value)
+    await localStorage.setItem('color2',color1.value)
+    await document.documentElement.style.setProperty("--foreground", localStorage.getItem('color2'));
+    await document.documentElement.style.setProperty("--background", localStorage.getItem('color1'));
+    window.location.reload()
+  });
+})
+
+
+
+
+/* NAVBAR JS */
+/* checkbox close on clicking outside of it */
+const checkboxToggle = document.getElementById('checkbox_toggle');
+const navbar = document.querySelector('.navbar');
+
+document.addEventListener('click', function(event) {
+  if (!navbar.contains(event.target)) {
+    checkboxToggle.checked = false;
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+async function gohome(){
+  window.location.replace('index.html')
+};
+
+
+/* typewriter effect for welcome message */
+async function welcomeMessage(){
+  var welcomeMessage = 'take a look around'
+  var header = document.getElementById('profileHeader')
+  header.innerHTML = '';
+
+  for (var i = 0; i < welcomeMessage.length; i++) {
+    (function(i) {
+      setTimeout(function() {
+        header.innerHTML += welcomeMessage.charAt(i);
+      }, i*125);
+    })(i);
+  }
+}
+welcomeMessage()
+
+
+/* CAROUSEL */
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselSlides = document.querySelectorAll('.carousel-slide');
+const carouselPrev = document.querySelector('.carousel-prev')
+const carouselNext = document.querySelector('.carousel-next')
+
+let currentPosition = 0;
+const slideWidth = carouselSlides[0].offsetWidth + 500;
+
+function moveToSlide(index) {
+  carouselContainer.style.transform = `translateX(calc(-${index * slideWidth}px))`;
+  carouselPrev.style.transform = `translateX(calc(${index * slideWidth}px))`;
+  carouselNext.style.transform = `translateX(calc(${index * slideWidth}px))`;
+  carouselPrev.style.top = `39%`;
+  carouselNext.style.top = `39%`;
+  currentPosition = index;
+}
+
+function moveRight() {
+  if (currentPosition < carouselSlides.length - 1) {
+    currentPosition++;
+    moveToSlide(currentPosition);
+  }
+}
+
+function moveLeft() {
+  if (currentPosition > 0) {
+    currentPosition--;
+    moveToSlide(currentPosition);
+  }
+}
+
+document.querySelector('.carousel-prev').addEventListener('click', moveLeft);
+document.querySelector('.carousel-next').addEventListener('click', moveRight);
+
